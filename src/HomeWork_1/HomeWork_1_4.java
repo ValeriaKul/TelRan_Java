619b08b0872c213b56538831a0ae2f028c98bf25
ValeriaKul <lera.dribnyuk@mail.ru>
2022-05-19 14:29:18 +0300

    Добавление переменной

--- src/HomeWork_1/HomeWork_1_4.java
+++ src/HomeWork_1/HomeWork_1_4.java
@@ -2,12 +2,13 @@ package HomeWork_1;
 
 public class HomeWork_1_4 {
     public static void main(String[] args) {
+        int calories = 40;
         int diameterFirstPizza = 24;
         int diameterSecondPizza = 28;
         int radiusFirstPizza = diameterFirstPizza/2;
         int radiusSecondPizza = diameterSecondPizza/2;
-        int calories = 40;
-        double extraCalories = (Math.PI *radiusSecondPizza*radiusSecondPizza)*calories - (Math.PI *radiusFirstPizza*radiusFirstPizza)*calories;
+        double differentSquare = (Math.PI *radiusSecondPizza*radiusSecondPizza) - (Math.PI *radiusFirstPizza*radiusFirstPizza);
+        double extraCalories = differentSquare*calories;
         System.out.println("Extra calories = " + extraCalories);
     }
 }
