140466fce6ab640bd2bb0f1d46bf8be97d1ee549
ValeriaKul <lera.dribnyuk@mail.ru>
2022-05-30 10:52:53 +0300

    HomeWork_3

--- src/HomeWork_3/Task_1.java
+++ src/HomeWork_3/Task_1.java
@@ -3,8 +3,7 @@ package HomeWork_3;
 public class Task_1 {
     /*Напишите метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
     где a, b, c, d — целочисленные входные параметры этого метода.*/
-    public int mathOperation1(int a, int b, int c, int d) {
-        int mathOperationResult = a * (b + (c / d));
-        return mathOperationResult;
+    public double mathOperation1(int a, int b, int c, int d) {
+        return (a * (b + ((double)c / d)));
     }
 }
